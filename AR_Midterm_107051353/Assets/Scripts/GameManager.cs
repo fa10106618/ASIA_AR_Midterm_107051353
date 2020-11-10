69d99471b098695791c99afa279abb1f64d072a6
fa10106618 <71679881+fa10106618@users.noreply.github.com>
2020-11-10 17:06:33 +0800

    2020/11/10期中完成

--- AR_Midterm_107051353/Assets/Scripts/GameManager.cs
+++ AR_Midterm_107051353/Assets/Scripts/GameManager.cs
@@ -13,7 +13,13 @@ public class GameManager : MonoBehaviour
     [Header("旋轉速度"), Range(0.1f, 10f)]
     public float turn = 1.5f;
     [Header("縮放"), Range(0f, 5f)]
-    public float size = 1.5f;
+    public float size = 0.3f;
+    [Header("怪人動畫元件")]
+    public Animator aniSlender;
+    [Header("女孩動畫元件")]
+    public Animator aniGirl;
+
+
 
     private void Start()
     {
@@ -29,6 +35,15 @@ public class GameManager : MonoBehaviour
 
         slender.localScale += new Vector3(1, 1, 1) * joystick.Vertical * size;
         girl.localScale += new Vector3(1, 1, 1) * joystick.Vertical * size;
-    }
 
+        slender.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(slender.localScale.x, 0.5f, 3.5f);
+        girl.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(girl.localScale.x, 0.5f, 3.5f);
+
+    }
+    public void playAnimation(string aniNeme) 
+    {
+        print(aniNeme);
+        aniSlender.SetTrigger(aniNeme);
+        aniGirl.SetTrigger(aniNeme);
+    }
 }
